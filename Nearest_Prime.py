00f7f3b2155f6ffae30de40a53b27e5d3216713d
Vinuthi <98702539+Vinuthi@users.noreply.github.com>
2022-07-01 22:55:03 +0530

    def prime(j):     i=2     v=0     while i!=j:         if j%i==0:             v=1         i+=1     if v==0:         return j x = int(input()) for i in range(x):     y = int(input())     b=y     for j in range(y,1,-1):         if prime(j):             n=j             break     for k in range(2,y):         if prime(b):             m=b             break         b+=1     if (y-n)<(m-y):         print(n)     elif (y-n)==(m-y):         print(n)     else:         print(m)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,4 +1,4 @@
-def is_prime(j):
+def prime(j):
     i=2
     v=0
     while i!=j:
@@ -7,18 +7,16 @@ def is_prime(j):
         i+=1
     if v==0:
         return j
-
-
 x = int(input())
 for i in range(x):
     y = int(input())
     b=y
-    for j in range(y,2-1,-1):
-        if is_prime(j):
+    for j in range(y,1,-1):
+        if prime(j):
             n=j
             break
-    while b!=0:
-        if is_prime(b):
+    for k in range(2,y):
+        if prime(b):
             m=b
             break
         b+=1
